86ee9a7cc79e6abd419bdc30749a620c84e2e748
Solene Baudic <solene.baudic@gmail.com>
2024-07-04 11:35:00 +0200

    list - routes/controller/views done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  before_action :set_lists, only: [:show]
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_list)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_lists
+    @list = List.find(params[:id])
+  end
+
+  def params_list
+    params.require(:list).permit(:name)
+  end
+end
